b9427566f77a65ab78c5065c52bea726201a3e4c
unknown <hemanthsuryavj0@gmail.com>
2021-03-24 19:39:56 +0530

    win game

--- Snake.java
+++ Snake.java
@@ -3,38 +3,58 @@ public class Snake
 {
 	public static void main(String[] args)
 	{
-		int WIN_POINT = 100;
-		int playerPosition = 0;
+		 Random random = new Random();
+        	 int WIN_POINT = 100;
+
+         public static void StartGame()
+         {
+            	int playerPosition = 0, dicevalue = 0;
+            	int playerDiceCount = 0;
+                while (true)
+               {
+                dicevalue = RollDie();
+                playerDiceCount++;
+                playerPosition = CalculatePlayerPosition(playerPosition, dicevalue);
+                System.out.println("Player position" + playerPosition);
+                if (playerPosition == WIN_POINT)
+                {
+                    System.out.println("player 1 wins");
+                    System.out.println("Die rolled " + playerDiceCount + " times to win");
+                    break;
+                }
+            }
+
 		int Dice = 6;
 		int roll = (int) (Math.random() * Dice) + 1;
 		System.out.println(roll);
-	}
 
-	public int calculateplayerPosition(int player, int diceValue)
-	{
-		int checkPlay = 3;
-		int check =(int) (Math.random() * checkPlay);
-
-		switch(check)
-		{
-			case 0:
-			System.out.println("No play");
-			break;
-
-			case 1:
-			System.out.println("ladder");
-			player = player + diceValue;
-			break;
-
-			default:
-			System.out.println("Snake");
-			player = player - diceValue;
-			break;
-		}
-		if(player < 0);
-		{
-			player = 0;
-		}
-		return player;
+        	public int CalculatePlayerPosition(int player,int diceValue)
+        	{
+            		int checkPlay;
+            		checkPlay = random.Next(3);
+            		switch (checkPlay)
+            		{
+                		case 0:
+                    			System.out.println("No play");
+                    			break;
+                		case 1:
+                    			System.out.println("ladder");
+                    			player = player + diceValue;
+                    			break;
+                		default:
+                    			System.out.println("Snake");
+                    			player = player - diceValue;
+                    			break;
+            		}
+            		if (player < 0)
+            		{
+                		player = 0;
+            		}
+            		if (player > WIN_POINT)
+            		{
+                		player = player - diceValue;
+            		}
+            		return player;
+        		}
 	}
 }
