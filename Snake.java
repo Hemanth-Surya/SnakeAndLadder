045a548629dedb9dad17f6daac60cdaf50fe92ca
unknown <hemanthsuryavj0@gmail.com>
2021-03-24 17:57:05 +0530

    roll dice

--- Snake.java
+++ Snake.java
@@ -2,6 +2,8 @@ public class Snake
 {
 	public static void main(String[] args)
 	{
-		int playerPosition = 0;
+		int Dice = 6;
+		int roll = (int) (Math.random() * Dice) + 1;
+		System.out.println(roll);
 	}
 }
